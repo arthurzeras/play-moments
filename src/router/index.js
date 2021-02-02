e22ba7c653b019d986f6f333eb9871f9c88c99c7
Arthur Oliveira <arthurolvmorais@gmail.com>
2021-02-02 19:43:21 -0300

    feat(frontend): create frontend application

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
+  },
+];
+
+const router = createRouter({
+  routes,
+  history: createWebHistory(process.env.BASE_URL),
+});
+
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <img alt="Play moments logo" src="../assets/img/logo.png" />
+    play moments
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
