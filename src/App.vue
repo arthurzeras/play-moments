3b0b791028f0810b48b6a33dd1bff5714279a044
Arthur Oliveira <arthurolvmorais@gmail.com>
2021-02-06 15:52:54 -0300

    feat(loading): Create loading component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <template>
-  <app-error v-if="hasError" />
+  <app-loading v-if="loading" />
+  <app-error v-else-if="hasError" />
   <router-view v-else />
 </template>
 
 <script setup>
+/* eslint-disable no-unused-vars */
 import store from '@/store';
 import { onMounted, ref } from 'vue';
 import { fetchObjects } from './services';
-// eslint-disable-next-line no-unused-vars
 import AppError from './components/AppError';
+import AppLoading from './components/AppLoading';
 
+const loading = ref(false);
 const hasError = ref(false);
 
 async function handleFetch() {
   try {
-    hasError.value = false;
+    loading.value = true;
     const data = await fetchObjects();
 
-    store.mutations.setGames(data.items);
+    store.mutations.setObjects(data.items);
   } catch (error) {
     hasError.value = true;
+  } finally {
+    loading.value = false;
   }
 }
 
---
+++ src/components/AppLoading.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="app-loading">
+    <img src="@/assets/img/logo.png" alt="Loading logo" />
+    <h1>Loading...</h1>
+  </section>
+</template>
+
+<style lang="scss">
+.app-loading {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: absolute;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  animation: blink infinite linear 2s;
+
+  img {
+    width: 45%;
+  }
+
+  h1 {
+    margin: 0;
+    font-size: 3rem;
+    color: var(--main);
+  }
+}
+
+@media (min-width: 768px) {
+  .app-loading img {
+    width: 30%;
+  }
+}
+
+@media (min-width: 1024px) {
+  .app-loading img {
+    width: 280px;
+  }
+}
+
+@keyframes blink {
+  from {
+    opacity: 0.5;
+  }
+
+  50% {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 0.5;
+  }
+}
+</style>
