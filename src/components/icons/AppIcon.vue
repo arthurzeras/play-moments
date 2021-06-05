954fa12553ed69dcd7872a3c699a0c3233fb6428
Arthur Oliveira <arthurolvmorais@gmail.com>
2021-06-05 15:33:36 -0300

    feat(icons): define global icon component

---
+++ src/components/icons/AppIcon.vue
@@ -0,0 +1,40 @@
+<template>
+  <svg
+    fill="none"
+    class="app-icon"
+    stroke="currentColor"
+    :viewBox="`0 0 ${icon.size} ${icon.size}`"
+  >
+    <path
+      :d="icon.path"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script>
+import icons from './icons';
+
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+
+  computed: {
+    icon() {
+      return icons.find(icon => icon.name === this.name) || {};
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.app-icon {
+  display: inline-block;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import App from './App.vue';
 import router from './router';
 import { createApp } from 'vue';
 import './assets/scss/main.scss';
+import AppIcon from './components/icons/AppIcon.vue';
 
 createApp(App)
+  .component('app-icon', AppIcon)
   .use(router)
   .mount('#app');
